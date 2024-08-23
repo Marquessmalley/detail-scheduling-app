7afd0fa2b23e36ba3eff9ad6daca44c5d4479b50
Marques Smalley <marquessmalley@gmail.com>
2024-08-23 11:22:09 -0400

    updated nav constant

--- src/constants/navmenu.ts
+++ src/constants/navmenu.ts
@@ -1,11 +1,19 @@
 import { Navigation } from "constants/interfaces";
 
 export const navigation: Navigation[] = [
-  { name: "Services", href: "#services", current: true },
+  {
+    name: "Services",
+    href: "https://main--ptshinetime.netlify.app/#services",
+    current: true,
+  },
   {
     name: "Pricing",
     href: "https://main--ptshinetime.netlify.app/#pricing",
     current: false,
   },
-  { name: "FAQs", href: "#faq", current: false },
+  {
+    name: "https://main--ptshinetime.netlify.app/#FAQs",
+    href: "#faq",
+    current: false,
+  },
 ];
